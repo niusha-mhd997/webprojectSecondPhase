8a3f3fe7c95cffcc73b575c5d8cfe45ec866a746
niusha-mhd997 <49919673+niusha-mhd997@users.noreply.github.com>
2019-05-23 18:08:14 +0430

    Add files via upload

---
+++ src/main/webapp/js/Login.js
@@ -0,0 +1,72 @@
+/**
+ * Created by win_8.1 on 5/21/2019.
+ */
+function Login() {
+
+    localStorage.setItem("name", "0");
+    localStorage.setItem("email", "0");
+    localStorage.setItem("password", "0");
+    localStorage.setItem("token", "0");
+   // alert("clicked")
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:8080/contacts/rest/contact/login",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "cache-control": "no-cache"
+        },
+        "data": {
+            "email": document.getElementById("namex").value,
+            "password": document.getElementById("pwd1x").value
+        }
+    };
+
+
+    $.ajax(settings).done(function (response) {
+
+       // alert(response.success + "  " + response.message);
+
+        if(response.message === "کاربری با این مشخصات یافت نشد"){
+            alert("کاربری با این مشخصات یافت نشد")
+        }
+
+        if(response.message === "رمز عبور اشتباه است"){
+            alert("رمز عبور اشتباه است")
+        }
+
+        if(response.message === "کارمندی شما تایید نشده"){
+            alert("متاسفانه هنوز کارمندی شما تایید نشده!")
+        }
+
+        if(response.message === "به عنوان دانشجو وارد شدید"){
+
+            localStorage.setItem("token",response.data.token);
+            localStorage.setItem("name",response.data.name);
+            localStorage.setItem("email",response.data.email);
+            localStorage.setItem("password",response.data.password);
+           // alert(response.data.token+ "  موفق");
+            window.open("http://localhost:8080/contacts/StudentPage.html","_parent");
+
+           // alert("به عنوان دانشجو وارد شدید");
+
+        }
+
+        if(response.message === "به عنوان مدیر وارد شدید"){
+        }
+
+        if(response.message === "به عنوان کارمند وارد شدید"){
+            alert("به عنوان کارمند وارد شدید");
+        }
+
+    });
+
+    $.ajax(settings).fail(function (jqXHR, textStatus) {
+        alert("error: " + jqXHR + "  " + textStatus);
+    });
+
+
+
+}
---
+++ src/main/webapp/js/Register.js
@@ -0,0 +1,58 @@
+/**
+ * Created by win_8.1 on 5/21/2019.
+ */
+function Register() {
+
+    //  console.log("amad");
+
+    if (document.getElementById("pwd1").value === document.getElementById("pwd2").value) {
+
+        if (document.getElementById("pwd1").value.length >= 6) {
+
+            localStorage.setItem("name", "0");
+            localStorage.setItem("email", "0");
+            localStorage.setItem("password", "0");
+            localStorage.setItem("token", "0");
+
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "http://localhost:8080/contacts/rest/student/studentregister",
+                "method": "POST",
+                "headers": {
+                    "content-type": "application/x-www-form-urlencoded",
+                    "cache-control": "no-cache"
+                },
+                "data": {
+                    "email": document.getElementById("email").value,
+                    "name": document.getElementById("name").value,
+                    "password": document.getElementById("pwd1").value
+                }
+            };
+
+            $.ajax(settings).done(function (response) {
+
+                if (response.success == true) {
+
+
+                    localStorage.setItem("token", response.data.token);
+                    localStorage.setItem("name", response.data.name);
+                    localStorage.setItem("email", response.data.email);
+                    localStorage.setItem("password", response.data.password);
+                    //alert(response.data.token+ "  موفق");
+                    window.open("http://localhost:8080/contacts/StudentPage.html", "_parent")
+
+                } else {
+                    alert(response.message)
+                }
+            });
+
+        }
+        else {
+            alert("طول رمز عبور باید حداقل 6 باشد")
+        }
+
+    } else {
+        alert("رمز عبور و تکرار با هم یکسان نیستند")
+    }
+}
