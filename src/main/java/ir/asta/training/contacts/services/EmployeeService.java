3067c37abccfe6cb7aecea3e9ee1c9a25b6ba855
Ehsan <ehsan.razazian98@gmail.com>
2019-06-14 12:17:16 +0430

    Add files via upload

---
+++ src/main/java/ir/asta/training/contacts/services/EmployeeService.java
@@ -0,0 +1,30 @@
+package ir.asta.training.contacts.services;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+import ir.asta.training.contacts.entities.*;
+import ir.asta.wise.core.datamanagement.ActionResult;
+import java.util.List;
+
+/**
+ * Created by win_8.1 on 6/13/2019.
+ */
+
+@Path("/employee")
+public interface EmployeeService {
+
+    @GET
+    @Path("/getallemployees")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<EmployeeEntity> getAllEmployees();
+
+
+    @POST
+    @Path("/employeeregister")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ActionResult<EmployeeEntity> register(@FormParam("email") String email,
+                                                @FormParam("name") String name,
+                                                @FormParam("semat") String semat,
+                                                @FormParam("password") String password);
+}
