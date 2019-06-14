853a6c77d98c7403de1394c4611da436de8f807e
Ehsan <ehsan.razazian98@gmail.com>
2019-06-14 12:20:51 +0430

    Update Register.js

--- src/main/webapp/js/Register.js
+++ src/main/webapp/js/Register.js
@@ -5,56 +5,127 @@ function Register() {
 
     //  console.log("amad");
 
-    if (document.getElementById("pwd1").value === document.getElementById("pwd2").value) {
+    if(document.getElementById("mySelect").value==="student"){
 
-        if (document.getElementById("pwd1").value.length >= 6) {
+        if (document.getElementById("pwd1").value === document.getElementById("pwd2").value) {
 
-            localStorage.setItem("name", "0");
-            localStorage.setItem("email", "0");
-            localStorage.setItem("password", "0");
-            localStorage.setItem("token", "0");
+            if (document.getElementById("pwd1").value.length >= 6) {
 
-
-            var settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": "rest/student/studentregister",
-                "method": "POST",
-                "headers": {
-                    "content-type": "application/x-www-form-urlencoded",
-                    "cache-control": "no-cache"
-                },
-                "data": {
-                    "email": document.getElementById("email").value,
-                    "name": document.getElementById("name").value,
-                    "password": document.getElementById("pwd1").value
-                }
-            };
-
-            $.ajax(settings).done(function (response) {
-
-                if (response.success == true) {
+                localStorage.setItem("name", "0");
+                localStorage.setItem("email", "0");
+                localStorage.setItem("password", "0");
+                localStorage.setItem("token", "0");
 
 
-                    localStorage.setItem("token", response.data.token);
-                    localStorage.setItem("name", response.data.name);
-                    localStorage.setItem("email", response.data.email);
-                    localStorage.setItem("password", response.data.password);
-                    localStorage.setItem("role", "student");
-                    //alert(response.data.token+ "  موفق");
-                    window.open("StudentPage.html", "_parent")
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "rest/student/studentregister",
+                    "method": "POST",
+                    "headers": {
+                        "content-type": "application/x-www-form-urlencoded",
+                        "cache-control": "no-cache"
+                    },
+                    "data": {
+                        "email": document.getElementById("email").value,
+                        "name": document.getElementById("name").value,
+                        "password": document.getElementById("pwd1").value
+                    }
+                };
 
-                } else {
-                    alert(response.message)
-                }
-            });
+                $.ajax(settings).done(function (response) {
 
+                    if (response.success == true) {
+
+
+                        localStorage.setItem("token", response.data.token);
+                        localStorage.setItem("name", response.data.name);
+                        localStorage.setItem("email", response.data.email);
+                        localStorage.setItem("password", response.data.password);
+                        localStorage.setItem("role", "student");
+                        //alert(response.data.token+ "  موفق");
+                        window.open("StudentPage.html", "_parent")
+
+                    } else {
+                        alert(response.message)
+                    }
+                });
+
+            }
+            else {
+                alert("طول رمز عبور باید حداقل 6 باشد")
+            }
+
+        } else {
+            alert("رمز عبور و تکرار با هم یکسان نیستند")
         }
-        else {
-            alert("طول رمز عبور باید حداقل 6 باشد")
-        }
 
-    } else {
-        alert("رمز عبور و تکرار با هم یکسان نیستند")
+
     }
+
+    else if(document.getElementById("mySelect").value==="employee") {
+
+        //alert("کارمند");
+
+        if (document.getElementById("pwd1").value === document.getElementById("pwd2").value) {
+
+            if (document.getElementById("pwd1").value.length >= 6) {
+
+                localStorage.setItem("name", "0");
+                localStorage.setItem("email", "0");
+                localStorage.setItem("password", "0");
+                localStorage.setItem("token", "0");
+
+
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "rest/employee/employeeregister",
+                    "method": "POST",
+                    "headers": {
+                        "content-type": "application/x-www-form-urlencoded",
+                        "cache-control": "no-cache"
+                    },
+                    "data": {
+                        "email": document.getElementById("email").value,
+                        "name": document.getElementById("name").value,
+                        "semat": "مسوول",
+                        "password": document.getElementById("pwd1").value
+                    }
+                };
+
+                $.ajax(settings).done(function (response) {
+
+                    if (response.success == true) {
+
+
+                        localStorage.setItem("token", response.data.token);
+                        localStorage.setItem("name", response.data.name);
+                        localStorage.setItem("email", response.data.email);
+                        localStorage.setItem("password", response.data.password);
+
+
+
+                        localStorage.setItem("role", "employee");
+                        window.open("EmployeePage.html", "_parent")
+
+
+                    } else {
+                        alert(response.message)
+                    }
+                });
+
+            }
+            else {
+                alert("طول رمز عبور باید حداقل 6 باشد")
+            }
+
+        } else {
+            alert("رمز عبور و تکرار با هم یکسان نیستند")
+        }
+
+    }
+
+
+
 }
