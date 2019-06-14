ac085f290e49fdaab184eaca927543c17e21bdc8
Ehsan <ehsan.razazian98@gmail.com>
2019-06-14 12:09:08 +0430

    Update EmployeeDao.java

--- src/main/java/ir/asta/training/contacts/dao/EmployeeDao.java
+++ src/main/java/ir/asta/training/contacts/dao/EmployeeDao.java
@@ -1,11 +1,13 @@
 package ir.asta.training.contacts.dao;
 
 import ir.asta.training.contacts.entities.EmployeeEntity;
-import ir.asta.training.contacts.entities.StudentEntity;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 /**
  * Created by win_8.1 on 6/5/2019.
@@ -18,7 +20,11 @@ public class EmployeeDao {
     private EntityManager entityManager;
 
 
-    public EmployeeEntity getEmployeeByEmail(String email){
+    @Inject
+    ContactDao contactDao;
+
+
+    public EmployeeEntity getEmployeeByEmail(String email) {
         String entityName = "EmployeeEntity";
         return (EmployeeEntity) entityManager.createQuery("select e from " + entityName + " e where e.email = :email")
                 .setParameter("email", email)
@@ -27,4 +33,24 @@ public class EmployeeDao {
 
     }
 
+    public List<EmployeeEntity> getAllEmployees() {
+        String entityName = "EmployeeEntity";
+        String queryStr = "select new ir.asta.training.contacts.entities.EmployeeEntity(e.email, e.name, e.semat) from " + entityName + " e";
+        TypedQuery<EmployeeEntity> query = entityManager.createQuery(queryStr, EmployeeEntity.class);
+        List<EmployeeEntity> results = query.getResultList();
+        return results;
+    }
+
+
+    public boolean Register(EmployeeEntity entity) {
+
+        int result = contactDao.CheckMail(entity.getEmail());
+
+        if (result == 0) {
+            entityManager.persist(entity);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
