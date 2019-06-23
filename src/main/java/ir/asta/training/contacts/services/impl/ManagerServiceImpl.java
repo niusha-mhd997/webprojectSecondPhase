120a25b4299b4ba59930c9c07be242a60f0b15ba
Ehsan <ehsan.razazian98@gmail.com>
2019-06-23 16:45:52 +0430

    Add files via upload

---
+++ src/main/java/ir/asta/training/contacts/services/impl/ManagerServiceImpl.java
@@ -0,0 +1,36 @@
+package ir.asta.training.contacts.services.impl;
+
+
+import ir.asta.training.contacts.entities.EmployeeEntity;
+import ir.asta.training.contacts.manager.ManagerManager;
+import ir.asta.training.contacts.manager.StudentManager;
+import ir.asta.training.contacts.services.ManagerService;
+import ir.asta.training.contacts.services.StudentService;
+import ir.asta.wise.core.datamanagement.ActionResult;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named("managerService")
+public class ManagerServiceImpl implements ManagerService {
+
+    @Inject
+    ManagerManager manager;
+
+    @Override
+    public ActionResult<String> AcceptEmployee(int employeetoken) {
+        String respons = manager.AcceptEmployee(employeetoken);
+        return new ActionResult<String>(true, respons, null);
+    }
+
+    @Override
+    public List<EmployeeEntity> getAllNotPermittedEmployees() {
+        return manager.getAllNotPermittedEmployees();
+    }
+
+    @Override
+    public List<EmployeeEntity> getAllPermittedEmployees() {
+        return manager.getAllPermittedEmployees();
+    }
+}
