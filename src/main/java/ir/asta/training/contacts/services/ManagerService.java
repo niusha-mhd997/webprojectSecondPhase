ce89aa2b34ad9e6563260e99ccdc70e29b96f110
Ehsan <ehsan.razazian98@gmail.com>
2019-06-23 16:46:50 +0430

    Add files via upload

---
+++ src/main/java/ir/asta/training/contacts/services/ManagerService.java
@@ -0,0 +1,33 @@
+package ir.asta.training.contacts.services;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import ir.asta.training.contacts.entities.CaseEntity;
+import ir.asta.training.contacts.entities.ContactEntity;
+import ir.asta.training.contacts.entities.EmployeeEntity;
+import ir.asta.training.contacts.entities.StudentEntity;
+import ir.asta.wise.core.datamanagement.ActionResult;
+
+import java.util.List;
+
+@Path("/manager")
+public interface ManagerService {
+
+    @POST
+    @Path("/acceptemployee")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ActionResult<String> AcceptEmployee(@FormParam("employeetoken") int employeetoken);
+
+
+    @GET
+    @Path("/getnotacceptedemps")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<EmployeeEntity> getAllNotPermittedEmployees();
+
+    @GET
+    @Path("/getnacceptedemps")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<EmployeeEntity> getAllPermittedEmployees();
+
+}
