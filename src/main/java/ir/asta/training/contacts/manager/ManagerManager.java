ae5dcb19f2bd68d440ee37f7871df1d8bb834f82
Ehsan <ehsan.razazian98@gmail.com>
2019-06-23 16:44:54 +0430

    Add files via upload

---
+++ src/main/java/ir/asta/training/contacts/manager/ManagerManager.java
@@ -0,0 +1,30 @@
+package ir.asta.training.contacts.manager;
+
+import ir.asta.training.contacts.dao.ManagerDao;
+import ir.asta.training.contacts.dao.StudentDao;
+import ir.asta.training.contacts.entities.EmployeeEntity;
+import ir.asta.wise.core.datamanagement.ActionResult;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named("managerManager")
+public class ManagerManager {
+
+    @Inject
+    ManagerDao dao;
+
+    public String AcceptEmployee(int employeetoken) {
+        return dao.AcceptEmployee(employeetoken);
+    }
+
+    public List<EmployeeEntity> getAllPermittedEmployees(){
+        return dao.getAllPermittedEmployees();
+    }
+
+    public List<EmployeeEntity> getAllNotPermittedEmployees(){
+        return dao.getAllNotPermittedEmployees();
+    }
+
+}
